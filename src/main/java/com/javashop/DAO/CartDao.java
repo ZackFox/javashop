8d15cc24887256837876858fb72ad017f3710fc8
Segey <firstthinker1989@gmail.com>
2017-03-31 17:39:58 +0500

    add to cart

--- src/main/java/com/javashop/DAO/CartDao.java
+++ src/main/java/com/javashop/DAO/CartDao.java
@@ -1,10 +1,6 @@
 package com.javashop.DAO;
 
-import com.javashop.model.Brand;
-import com.javashop.model.Cart;
 import com.javashop.model.CartItem;
-import com.javashop.model.Product;
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 
 import java.util.List;
 
